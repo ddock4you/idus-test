879edc97db9e6ef4ebe7180d858c76a309c920dc
ddock4you <ddock4you@gmail.com>
2020-06-01 02:17:52 +0900

    Updates

---
+++ precache-manifest.d8a125ff57ca5b2dd17ae93d620fc19b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d853fe82f76a0ef36c40ddb575631881",
+    "url": "/idus-test/index.html"
+  },
+  {
+    "revision": "70d4d66f7684a9637cd9",
+    "url": "/idus-test/static/css/main.fa8db7a2.chunk.css"
+  },
+  {
+    "revision": "98d8383a102f83494633",
+    "url": "/idus-test/static/js/2.efc46671.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/idus-test/static/js/2.efc46671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d4d66f7684a9637cd9",
+    "url": "/idus-test/static/js/main.d6c960de.chunk.js"
+  },
+  {
+    "revision": "807c6380d86a4aa59a14",
+    "url": "/idus-test/static/js/runtime-main.10e8cec1.js"
+  }
+]);
